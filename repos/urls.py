0b90e19906198d86dc0dd23532486489934387f2
anacrethus_alto <altoanacrethus@gmail.com>
2026-01-22 09:54:17 +0300

    set home_dashboard as root route in repos app

--- repos/urls.py
+++ repos/urls.py
@@ -4,7 +4,10 @@ from .import views
 app_name = 'repos'
 
 urlpatterns = [
-    path('', views.repo_list, name='list' ),
+    path('', views.home_dashboard, name='home'),
+    path('list/', views.repo_list, name='list' ),
     path('create/', views.repo_create, name='create'),
+    path('<int:repo_id>/', views.repo_detail, name='detail'),
+    
     
 ]
